25e412ca251ac62ca52e2059af9b1b09f508b529
Monisha-MS <monisha07sellamuthu@gmail.com>
2024-03-18 16:18:49 +0530

    initial commit

---
+++ config.mjs
@@ -0,0 +1,7 @@
+export default {
+    base: './',
+    build: {
+      outDir: 'build',
+      chunkSizeWarningLimit: 100000
+    }
+  };
---
+++ vite.config.js
@@ -0,0 +1,4 @@
+import { defineConfig } from 'vite';
+import config from './config.mjs';
+
+export default defineConfig(config);
